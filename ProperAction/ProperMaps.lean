2a1174b97ed0d4cfd86ae12f644bccbbfc36d452
Etienne <etienne.marion30@gmail.com>
2024-03-25 18:53:35 +0100

    proof of IsProperMap.comp

--- ProperAction/ProperMaps.lean
+++ ProperAction/ProperMaps.lean
@@ -11,4 +11,13 @@ variable {X Y Z : Type*} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalS
 
 lemma IsProperMap.comp {f : X → Y} {g : Y → Z} (hf : IsProperMap f) (hg : IsProperMap g) :
     IsProperMap (g ∘ f) := by
-  sorry
+    constructor
+    apply Continuous.comp hg.continuous hf.continuous
+    intro F z h
+    have ⟨y, hy1, hy2⟩ : ∃ y : Y, g y = z ∧ ClusterPt y (F.map f) := hg.clusterPt_of_mapClusterPt h
+    have ⟨x, hx1, hx2⟩ : ∃ x : X, f x = y ∧ ClusterPt x F := hf.clusterPt_of_mapClusterPt hy2
+    use x
+    constructor
+    rw [← hy1, ← hx1]
+    rfl
+    assumption
