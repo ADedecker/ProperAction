c983f39fcec7a6ad524b9d030989a0ebc97d7ef1
ADedecker <anatolededecker@gmail.com>
2024-03-25 21:09:46 +0100

    Fix indentation

--- ProperAction/ProperMaps.lean
+++ ProperAction/ProperMaps.lean
@@ -11,14 +11,14 @@ variable {X Y Z : Type*} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalS
 
 lemma IsProperMap.comp {f : X → Y} {g : Y → Z} (hf : IsProperMap f) (hg : IsProperMap g) :
     IsProperMap (g ∘ f) := by
-    constructor
-    apply hg.continuous.comp hf.continuous
-    intro F z h
-    rw [MapClusterPt, ← Filter.map_map] at h
-    rcases hg.clusterPt_of_mapClusterPt h with ⟨y, hy1, hy2⟩
-    rcases hf.clusterPt_of_mapClusterPt hy2 with ⟨x, hx1, hx2⟩
-    use x
-    constructor
-    rw [← hy1, ← hx1]
-    rfl
-    assumption
+  constructor
+  apply hg.continuous.comp hf.continuous
+  intro F z h
+  rw [MapClusterPt, ← Filter.map_map] at h
+  rcases hg.clusterPt_of_mapClusterPt h with ⟨y, hy1, hy2⟩
+  rcases hf.clusterPt_of_mapClusterPt hy2 with ⟨x, hx1, hx2⟩
+  use x
+  constructor
+  rw [← hy1, ← hx1]
+  rfl
+  assumption
